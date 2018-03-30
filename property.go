ca43b101120038443899892a14a59f5b5f38b29d
Kriz, Jim (NonEmp) <jim.kriz@kroger.com>
2018-03-30 16:23:01 -0400

    Handle ast.StarExpr with a recursive call

--- property.go
+++ property.go
@@ -8,7 +8,12 @@ import (
 // getPropertyName returns the string value for the given field if it exists, otherwise it panics.
 // allowedValues: array, boolean, integer, null, number, object, string
 func getPropertyName(field *ast.Field) (name string, fieldType string) {
-	if astTypeSelectorExpr, ok := field.Type.(*ast.SelectorExpr); ok {
+	return getTypeName(field.Type)
+
+}
+
+func getTypeName(expr ast.Expr) (name string, fieldType string) {
+	if astTypeSelectorExpr, ok := expr.(*ast.SelectorExpr); ok {
 
 		// Support for time.Time as a structure field
 		if "Time" == astTypeSelectorExpr.Sel.Name {
@@ -22,24 +27,25 @@ func getPropertyName(field *ast.Field) (name string, fieldType string) {
 
 		panic("not supported 'astSelectorExpr' yet.")
 
-	} else if astTypeIdent, ok := field.Type.(*ast.Ident); ok {
+	} else if astTypeIdent, ok := expr.(*ast.Ident); ok {
 		name = astTypeIdent.Name
 
 		// When its the int type will transfer to integer which is goswagger supported type
 		schemeType := TransToValidSchemeType(name)
 		return schemeType, schemeType
 
-	} else if _, ok := field.Type.(*ast.StarExpr); ok {
-		panic("not supported astStarExpr yet.")
-	} else if _, ok := field.Type.(*ast.MapType); ok { // if map
+	} else if astTypeStar, ok := expr.(*ast.StarExpr); ok {
+		return getTypeName(astTypeStar.X)
+		//panic("not supported astStarExpr yet.")
+	} else if _, ok := expr.(*ast.MapType); ok { // if map
 		//TODO: support map
 		return "object", "object"
-	} else if astTypeArray, ok := field.Type.(*ast.ArrayType); ok { // if array
+	} else if astTypeArray, ok := expr.(*ast.ArrayType); ok { // if array
 		str := fmt.Sprintf("%s", astTypeArray.Elt)
 		return "array", str
-	} else if _, ok := field.Type.(*ast.StructType); ok { // if struct
+	} else if _, ok := expr.(*ast.StructType); ok { // if struct
 		return "object", "object"
-	} else if _, ok := field.Type.(*ast.InterfaceType); ok { // if interface{}
+	} else if _, ok := expr.(*ast.InterfaceType); ok { // if interface{}
 		return "object", "object"
 	}
 
